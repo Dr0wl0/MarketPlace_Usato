4950640d880959915fcdb28e34e979fad6caa8a6
Dr0wl0 <massimobaffa850@gmail.com>
2025-07-11 17:10:41 +0200

    Generazione del LogIn_Registration service

---
+++ MarketPlace_Usato_BackEnd/LogIn_Registration-service/LogIn_Registration-service/src/main/java/it/profice/corso/LogIn_Registration_service/LogInRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package it.profice.corso.LogIn_Registration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInRegistrationServiceApplication.class, args);
+	}
+
+}
