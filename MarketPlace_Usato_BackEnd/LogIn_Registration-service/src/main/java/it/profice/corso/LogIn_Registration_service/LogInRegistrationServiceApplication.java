0034c716ce1d253e8e8551e017a8b9523b9fa7e8
Markkief <chieffa33@gmail.com>
2025-07-12 23:16:33 +0200

    add discovery server + api gateway

--- MarketPlace_Usato_BackEnd/LogIn_Registration-service/src/main/java/it/profice/corso/LogIn_Registration_service/LogInRegistrationServiceApplication.java
+++ MarketPlace_Usato_BackEnd/LogIn_Registration-service/src/main/java/it/profice/corso/LogIn_Registration_service/LogInRegistrationServiceApplication.java
@@ -2,8 +2,10 @@ package it.profice.corso.LogIn_Registration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("/http://localhost:4200")
 public class LogInRegistrationServiceApplication {
 
 	public static void main(String[] args) {
