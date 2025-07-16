ba3b34db6e4a1a87f9ce166907f136ed7c271feb
Dr0wl0 <massimobaffa850@gmail.com>
2025-07-16 13:46:45 +0200

    Cart-service  FIXED

--- MarketPlace_Usato_BackEnd/cart-service/src/main/java/it/profice/corso/cart_service/repositroy/CartItemRepositroy.java
+++ MarketPlace_Usato_BackEnd/cart-service/src/main/java/it/profice/corso/cart_service/repositroy/CartItemRepositroy.java
@@ -1,5 +1,7 @@
 package it.profice.corso.cart_service.repositroy;
 
+import it.profice.corso.cart_service.DTO.CartDTO;
+import it.profice.corso.cart_service.model.Cart;
 import it.profice.corso.cart_service.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
